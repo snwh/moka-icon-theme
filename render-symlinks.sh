e2f32f294977f5fd76ad64ba4b24ae0ee7bf5631
Sam Hewitt <hewittsamuel@gmail.com>
2013-05-28 01:01:16 -0400

    Reverted Sparkleshare icon; added symlinks (and script for it); etc.

---
+++ render-symlinks.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+cd Moka
+
+cd 16x16
+icontool-map -c actions
+icontool-map -c apps
+icontool-map -c categories
+icontool-map -c devices
+icontool-map -c mimetypes
+icontool-map -c places
+icontool-map -c status
+cd ..
+cd 22x22
+icontool-map -c actions
+icontool-map -c apps
+icontool-map -c categories
+icontool-map -c devices
+icontool-map -c mimetypes
+icontool-map -c places
+icontool-map -c status
+cd ..
+cd 24x24
+icontool-map -c actions
+icontool-map -c apps
+icontool-map -c categories
+icontool-map -c devices
+icontool-map -c mimetypes
+icontool-map -c places
+icontool-map -c status
+cd ..
+cd 32x32
+icontool-map -c actions
+icontool-map -c apps
+icontool-map -c categories
+icontool-map -c devices
+icontool-map -c mimetypes
+icontool-map -c places
+icontool-map -c status
+cd ..
+cd 48x48
+icontool-map -c actions
+icontool-map -c apps
+icontool-map -c categories
+icontool-map -c devices
+icontool-map -c mimetypes
+icontool-map -c places
+icontool-map -c status
+cd ..
+cd 64x64
+icontool-map -c actions
+icontool-map -c apps
+icontool-map -c categories
+icontool-map -c devices
+icontool-map -c mimetypes
+icontool-map -c places
+icontool-map -c status
+cd ..
+cd 96x96
+icontool-map -c actions
+icontool-map -c apps
+icontool-map -c categories
+icontool-map -c devices
+icontool-map -c mimetypes
+icontool-map -c places
+icontool-map -c status
+cd ..
+cd 256x256
+icontool-map -c actions
+icontool-map -c apps
+icontool-map -c categories
+icontool-map -c devices
+icontool-map -c mimetypes
+icontool-map -c places
+icontool-map -c status
+cd ..
+cd 16x16
+icontool-map -c actions
+icontool-map -c apps
+icontool-map -c categories
+icontool-map -c devices
+icontool-map -c mimetypes
+icontool-map -c places
+icontool-map -c status
+cd ..
