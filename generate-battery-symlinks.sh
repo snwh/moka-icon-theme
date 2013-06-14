4e9bccccb7b151eae78c134264ecd03727ca4335
Sam Hewitt <hewittsamuel@gmail.com>
2013-06-14 09:55:21 -0400

    Added icons; added battery symlinks.

---
+++ generate-battery-symlinks.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+cd Moka
+
+# Generate symlinks for 16x16 icons
+cd 16x16/status
+echo 'Generating links for 16x16 pixel symbolic battery icons.'
+ln -sf battery-empty.png battery-empty-symbolic.png
+ln -sf battery-empty-charging.png battery-empty-charging-symbolic.png
+ln -sf battery-full.png battery-full-symbolic.png
+ln -sf battery-full-charged.png battery-full-charged-symbolic.png
+ln -sf battery-full-charging.png battery-full-charging-symbolic.png
+ln -sf battery-good.png battery-good-symbolic.png
+ln -sf battery-good-charging.png battery-good-charging-symbolic.png
+ln -sf battery-low.png battery-low-symbolic.png
+ln -sf battery-low-charging.png battery-low-charging-symbolic.png
+ln -sf battery-medium.png battery-medium-symbolic.png
+ln -sf battery-medium-charging.png battery-medium-charging-symbolic.png
+ln -sf battery-missing.png battery-missing-symbolic.png
+echo 'Done.'
+cd ..
+cd ..
+
+# Generate symlinks for 22x22 icons
+cd 22x22/status
+echo 'Generating links for 22x22 pixel symbolic battery icons.'
+ln -sf battery-empty.png battery-empty-symbolic.png
+ln -sf battery-empty-charging.png battery-empty-charging-symbolic.png
+ln -sf battery-full.png battery-full-symbolic.png
+ln -sf battery-full-charged.png battery-full-charged-symbolic.png
+ln -sf battery-full-charging.png battery-full-charging-symbolic.png
+ln -sf battery-good.png battery-good-symbolic.png
+ln -sf battery-good-charging.png battery-good-charging-symbolic.png
+ln -sf battery-low.png battery-low-symbolic.png
+ln -sf battery-low-charging.png battery-low-charging-symbolic.png
+ln -sf battery-medium.png battery-medium-symbolic.png
+ln -sf battery-medium-charging.png battery-medium-charging-symbolic.png
+ln -sf battery-missing.png battery-missing-symbolic.png
+echo 'Done.'
+cd ..
+cd ..
+
+# Generate symlinks for 24x24 icons
+cd 24x24/status
+echo 'Generating links for 24x24 pixel symbolic battery icons.'
+ln -sf battery-empty.png battery-empty-symbolic.png
+ln -sf battery-empty-charging.png battery-empty-charging-symbolic.png
+ln -sf battery-full.png battery-full-symbolic.png
+ln -sf battery-full-charged.png battery-full-charged-symbolic.png
+ln -sf battery-full-charging.png battery-full-charging-symbolic.png
+ln -sf battery-good.png battery-good-symbolic.png
+ln -sf battery-good-charging.png battery-good-charging-symbolic.png
+ln -sf battery-low.png battery-low-symbolic.png
+ln -sf battery-low-charging.png battery-low-charging-symbolic.png
+ln -sf battery-medium.png battery-medium-symbolic.png
+ln -sf battery-medium-charging.png battery-medium-charging-symbolic.png
+ln -sf battery-missing.png battery-missing-symbolic.png
+echo 'Done.'
