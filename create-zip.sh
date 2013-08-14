3727a7dfd25f2bbeb6e32048d1d68ca519cf903d
Sam Hewitt <hewittsamuel@gmail.com>
2013-08-14 01:58:51 -0400

    Removed unnecessary files; updated create-zip.sh

--- create-zip.sh
+++ create-zip.sh
@@ -2,4 +2,4 @@
 
 cd /home/sam/dev/moka-icon-theme
 
-zip -r moka-icon-theme.zip Moka UNINSTALL INSTALL
+zip -r moka-icon-theme.zip Moka AUTHORS UNINSTALL INSTALL COPYING COPYING_CCBYSA3 COPYING_LGPL
