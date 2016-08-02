3cb678d56368130805366fad7b303e1385857b81
John Beard <john.j.beard@gmail.com>
2016-08-02 20:43:12 +0800

    Export all DPI factors at once, don't need two scripts

--- src/render-bitmaps.py
+++ src/render-bitmaps.py
@@ -27,6 +27,11 @@ OPTIPNG = '/usr/bin/optipng'
 MAINDIR = '../Moka'
 SOURCES = ('#', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'web', 'X', 'Y', 'Z')
 
+# the resolution that non-hi-dpi icons are rendered at
+DPI_1_TO_1 = 90
+# DPI multipliers to render at
+DPIS = [1, 2]
+
 inkscape_process = None
 
 def main(args, SRC):
@@ -56,11 +61,16 @@ def main(args, SRC):
         wait_for_prompt(process)
         return process
 
-    def inkscape_render_rect(icon_file, rect, output_file):
+    def inkscape_render_rect(icon_file, rect, dpi, output_file):
         global inkscape_process
         if inkscape_process is None:
             inkscape_process = start_inkscape()
-        wait_for_prompt(inkscape_process, '%s -i %s -e %s' % (icon_file, rect, output_file))
+
+        cmd = [icon_file,
+               '--export-dpi', str(dpi),
+               '-i', rect,
+               '-e', output_file]
+        wait_for_prompt(inkscape_process, ' '.join(cmd))
         optimize_png(output_file)
 
     class ContentHandler(xml.sax.ContentHandler):
@@ -137,27 +147,33 @@ def main(args, SRC):
 
                 print (self.context, self.icon_name)
                 for rect in self.rects:
-                    width = rect['width']
-                    height = rect['height']
-                    id = rect['id']
+                    for dpi_factor in DPIS:
+                        width = rect['width']
+                        height = rect['height']
+                        id = rect['id']
+                        dpi = DPI_1_TO_1 * dpi_factor
 
-                    dir = os.path.join(MAINDIR, "%sx%s" % (width, height), self.context)
-                    outfile = os.path.join(dir, self.icon_name+'.png')
-                    if not os.path.exists(dir):
-                        os.makedirs(dir)
-                    # Do a time based check!
-                    if self.force or not os.path.exists(outfile):
-                        inkscape_render_rect(self.path, id, outfile)
-                        sys.stdout.write('.')
-                    else:
-                        stat_in = os.stat(self.path)
-                        stat_out = os.stat(outfile)
-                        if stat_in.st_mtime > stat_out.st_mtime:
-                            inkscape_render_rect(self.path, id, outfile)
+                        size_str = "%sx%s" % (width, height)
+                        if dpi_factor != 1:
+                            size_str += "@%sx" % dpi_factor
+
+                        dir = os.path.join(MAINDIR, size_str, self.context)
+                        outfile = os.path.join(dir, self.icon_name+'.png')
+                        if not os.path.exists(dir):
+                            os.makedirs(dir)
+                        # Do a time based check!
+                        if self.force or not os.path.exists(outfile):
+                            inkscape_render_rect(self.path, id, dpi, outfile)
                             sys.stdout.write('.')
                         else:
-                            sys.stdout.write('-')
-                    sys.stdout.flush()
+                            stat_in = os.stat(self.path)
+                            stat_out = os.stat(outfile)
+                            if stat_in.st_mtime > stat_out.st_mtime:
+                                inkscape_render_rect(self.path, id, dpi, outfile)
+                                sys.stdout.write('.')
+                            else:
+                                sys.stdout.write('-')
+                        sys.stdout.flush()
                 sys.stdout.write('\n')
                 sys.stdout.flush()
 
